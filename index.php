5102df13a1ed8f124b572ea176d8d00b4d9bfd39
unknown <Administrador@Reserva11.paco.local>
2026-09-15 16:58:29 -0300

    rotas

--- index.php
+++ index.php
@@ -7,6 +7,6 @@ session_start();
 
 
 
-header("Location: /helpdesk_prefeitura/account/login.php");
+//header("Location: /helpdesk_prefeitura/account/login.php");
 //header("Location: /account/login.php");
 exit;
